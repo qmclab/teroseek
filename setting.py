054c5292ae59313d6aa2a2a7d038a3feb3c1f38c
qmclab <qmclab@163.com>
2025-07-31 14:02:33 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -3,10 +3,10 @@
 
 
 ARK_BASE_URL = "https://ark.cn-beijing.volces.com/api/v3"
-AEK_API_KEY = "104510e0-cf1d-447d-a2d1-a9d47407f1cb"
-DS_V3_ID = "ep-20250626163448-hrc8p"
-DS_R1_ID = "ep-20250626163310-rkbpc"
-DOUBAO_EMBEDDING_ID = "ep-20250626163553-ff2nq"
+AEK_API_KEY = ""
+DS_V3_ID = ""
+DS_R1_ID = ""
+DOUBAO_EMBEDDING_ID = ""
 
 
 
